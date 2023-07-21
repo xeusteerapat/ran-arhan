e26b4a8aeba281e8ed7c94143fff89e4d2f7bcd7
xeusteerapat <xeus085@gmail.com>
2023-07-21 22:39:12 +0700

    :tada: Init

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>OK this is cool</Text>
+      <Pressable onPress={() => alert('Heyyyy 👋')} style={styles.button}>
+        <Text style={styles.text}>Save</Text>
+      </Pressable>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -16,5 +19,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 5,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  header: {
+    fontSize: 24,
+    marginBottom: 6,
   },
 });
